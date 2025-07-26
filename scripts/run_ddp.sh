f245c6ad42e21a8da35a2fb3325bc18e13e1aa92
Philip Botros <philip@captur.ai>
2025-07-26 07:54:04 +0000

    chore: add torchrun launcher script

---
+++ scripts/run_ddp.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+
+# Launch script for main_ddp.py using torchrun
+# Usage: ./run_ddp.sh [num_gpus]
+
+NUM_GPUS=${1:-auto}  # Auto-detect GPUs if not specified
+
+echo "Launching DDP training with $NUM_GPUS GPUs..."
+
+torchrun \
+    --nproc_per_node=$NUM_GPUS \
+    --nnodes=1 \
+    --node_rank=0 \
+    --master_addr=localhost \
+    --master_port=29500 \
+    main_ddp.py
